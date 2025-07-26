c00d4ed8acdadb8d04bc151f00ac2bd90bf67ddb
Victor Widell <victor.widell@divid.se>
2025-07-26 16:43:28 +0200

    Implementation

--- src/interpret.ts
+++ src/interpret.ts
@@ -1,9 +1,26 @@
 export async function interpret(
-	_code: string,
-	_io: {
+	code: string,
+	io: {
 		get: () => Promise<number>;
 		put: (value: number) => void;
 	},
 ): Promise<void> {
-	return;
+	const instructions = createInstructions(code);
+
+	switch (instructions.getNext()) {
+		case undefined: {
+			return;
+		}
+
+		case ".": {
+			io.put(0);
+			break;
+		}
+	}
+}
+
+function createInstructions(code: string) {
+	return {
+		getNext: (): string | undefined => code[0],
+	};
 }
